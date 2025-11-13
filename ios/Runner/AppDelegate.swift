f9573456c30bca819b8e15889b219549b2f02ed3
Mahmoud Hassan <abosamour990@gmail.com>
2025-11-13 23:38:41 +0200

    m

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+// import Firebase  // Uncomment if using Firebase directly
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // FirebaseApp.configure()  // Uncomment if using Firebase directly
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
