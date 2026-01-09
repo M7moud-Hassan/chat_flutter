6244bb1276b80dcb4f4feae22d162ff97ec75101
Mahmoud Hassan <abosamour990@gmail.com>
2026-01-09 15:35:56 +0200

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -44,10 +44,10 @@ void main() async {
   await SharedPref.init();
 
   await DeviceStorage.init();
-  // final noScreenshot = NoScreenshot.instance;
+  final noScreenshot = NoScreenshot.instance;
 
-  // // // 🔒 Disable screenshots & screen recording (Android + iOS)
-  // await noScreenshot.screenshotOff();
+  // // 🔒 Disable screenshots & screen recording (Android + iOS)
+  await noScreenshot.screenshotOff();
 
   FirebaseMessaging.onBackgroundMessage(
     firebaseMessagingBackgroundHandler,
